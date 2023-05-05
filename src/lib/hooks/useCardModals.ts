443f6e2dc2df21a932125a573b9afb3046905d2c
turtle601 <turtle601@naver.com>
2023-05-05 20:14:47 +0900

    Compound Component로 모달 컴포넌트 추상화

---
+++ src/lib/hooks/useCardModals.ts
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export const useCardModal = (
+  defaultModal: boolean
+): [boolean, () => void, () => void] => {
+  const [isModal, setModal] = useState<boolean>(defaultModal);
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  return [isModal, closeModal, openModal];
+};
